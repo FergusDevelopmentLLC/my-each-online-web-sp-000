ca4eebea44dbe54688b926b6817510f0d0ac38cb
Will Carter <will.carter@fergusllc.com>
2019-12-20 20:10:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  array.each { |item|
+    yield
+  }
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
